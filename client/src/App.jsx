3a858fc2c041f5a38ccc0472dd5a089fd9a9434d
YoTi <yasserrafai5@gmail.com>
2024-06-18 16:26:21 +0100

    Hero Section and Search bar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,16 @@
 import Navbar from "./components/Navbar/Navbar";
 import "./layout.scss"
+import HomePage from "./routes/homePage/homePage";
 
 function App() {
   return (
     <div className="layout">
+      <div className="navbar">
       <Navbar/>
+      </div>
+      <div className="content">
+      <HomePage/>
+      </div>
     </div>
   )
 }
