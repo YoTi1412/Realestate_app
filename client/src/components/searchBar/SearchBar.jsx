7aeecc7e1b13fac516b0cae566cb636a902700ad
YoTi <yasserrafai5@gmail.com>
2024-06-20 17:02:57 +0100

    finish the frontend

--- client/src/components/searchBar/SearchBar.jsx
+++ client/src/components/searchBar/SearchBar.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import "./SearchBar.scss";
+import "./searchBar.scss";
 
 const types = ["buy", "rent"];
 
--- client/src/routes/homePage/homePage.jsx
+++ client/src/routes/homePage/homePage.jsx
@@ -8,12 +8,15 @@ function HomePage() {
         <div className="wrapper">
           <h1 className="title">Find Real Estate & Get Your Dream Place</h1>
           <p>
-          Welcome to our real estate sanctuary, where every listing is a gateway to possibility. Whether you're seeking a cozy nest or a sprawling estate, let us guide you to the home that speaks to your heart. Explore our meticulously curated collection of properties and uncover the perfect place to start your next chapter.
+          Welcome to our real estate sanctuary, where every listing is a gateway to possibility.
+          Whether you're seeking a cozy nest or a sprawling estate, let us guide you to the home that speaks to your heart.
+          Explore our meticulously curated collection of properties and uncover 
+          the perfect place to start your next chapter.
           </p>
           <SearchBar />
           <div className="boxes">
             <div className="box">
-              <h1>10+</h1>
+              <h1>16+</h1>
               <h2>Years of Experience</h2>
             </div>
             <div className="box">
