9237e76d3d28a552b673d8e096b328db7cd8b9d6
YoTi <yasserrafai5@gmail.com>
2024-06-21 20:24:38 +0100

    Implement Express.js JWT cookies authentication and React context for user auth
    - Added JWT-based authentication using cookies in Express.js
    - Protected Express.js routes using JWT cookies
    - Integrated React Context API for authentication management
    - Implemented user login, logout, and profile update functionalities in React
    - Updated profile page to dynamically display user information from context

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,18 +1,20 @@
 import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 import prisma from "../lib/prisma.js";
 
 export const register = async (req, res) =>
 {
+    const { username, email, password } = req.body;
+
     try
     {
-        const { username, email, password } = req.body;
+        // HASH THE PASSWORD
 
-        // HASH the Password
         const hashedPassword = await bcrypt.hash(password, 10);
 
         console.log(hashedPassword);
 
-        // Create a new user and save to DB
+        // CREATE A NEW USER AND SAVE TO DB
         const newUser = await prisma.user.create({
             data: {
                 username,
@@ -33,10 +35,10 @@ export const register = async (req, res) =>
 
 export const login = async (req, res) =>
 {
+    const { username, password } = req.body;
+
     try
     {
-        const { username, password } = req.body;
-
         // CHECK IF THE USER EXISTS
 
         const user = await prisma.user.findUnique({
@@ -54,7 +56,26 @@ export const login = async (req, res) =>
 
         // GENERATE COOKIE TOKEN AND SEND TO THE USER
 
-        
+        const age = 1000 * 60 * 60 * 24 * 7;
+
+        const token = jwt.sign(
+            {
+                id: user.id,
+                isAdmin: false,
+            },
+            process.env.JWT_SECRET_KEY,
+            { expiresIn: age }
+        );
+
+        const { password: userPassword, ...userInfo } = user;
+
+        res
+            .cookie("token", token, {
+                httpOnly: true,
+                maxAge: age,
+            })
+            .status(200)
+            .json(userInfo);
     } catch (err)
     {
         console.log(err);
@@ -64,6 +85,5 @@ export const login = async (req, res) =>
 
 export const logout = (req, res) =>
 {
-    // db operations
-    console.log("logout endpoint");
+    res.clearCookie("token").status(200).json({ message: "Logout Successful" });
 };
