08e96d5845276a6257ac0c801914937ce0554a2c
YoTi <yasserrafai5@gmail.com>
2024-06-28 21:19:43 +0100

    Completed CRUD operations with Express.js and Prisma, added avatar update functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,6 +7,7 @@ import ListPage from "./routes/listPage/listPage";
 import Layout from "./routes/layout/layout";
 import SinglePage from "./routes/singlePage/singlePage";
 import ProfilePage from "./routes/profilePage/profilePage";
+import ProfileUpdatePage from "./routes/profileUpdatePage/profileUpdatePage";
 import Login from "./routes/login/login";
 import Register from "./routes/register/register";
 
@@ -31,6 +32,10 @@ function App() {
         {
           path:"/profile",
           element:<ProfilePage/>
+        },
+        {
+          path: "/profile/update",
+          element: <ProfileUpdatePage />,
         },
         {
           path:"/login",
